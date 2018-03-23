233fd003d606b65c6b652df1a0e8146dc0f0ac1b
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-23 20:28:30 -0300

    Test passing

--- people/views.py
+++ people/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from .serializers import StudentSerializer
 
 class StudentViewSet(viewsets.ModelViewSet):
-    pass
+    serializer_class = StudentSerializer
