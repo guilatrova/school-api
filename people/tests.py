a630020fb0e462790ae0802cca89068657f2701e
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-23 20:15:36 -0300

    Refactor url tests

--- people/tests.py
+++ people/tests.py
@@ -2,31 +2,30 @@ from django.test import TestCase
 from django.urls import reverse, resolve
 from people import views
 
-def resolve_by_name(name, **kwargs):
-    url = reverse(name, kwargs=kwargs)
-    return resolve(url)
-
 class PeopleUrl(TestCase):
     def test_resolves_students_list_url(self):
-        resolver = resolve_by_name('students')
+        resolver = self.resolve_by_name('students')
         self.assertEqual(resolver.func.cls, views.StudentViewSet)
 
     def test_resolves_students_single_url(self):
-        resolver = resolve_by_name('student', id=1)
+        resolver = self.resolve_by_name('student', id=1)
         self.assertEqual(resolver.func.cls, views.StudentViewSet)
 
     def test_students_list_url_allows(self):
-        resolver = resolve_by_name('students')
+        resolver = self.resolve_by_name('students')
         allowed = ['get', 'post']
-        
-        self.assertEqual(len(allowed), len(resolver.func.actions))
-        for action in allowed:
-            self.assertIn(action, resolver.func.actions)
+        self.assert_has_actions(allowed, resolver.func.actions)
 
     def test_students_single_url_allows(self):
-        resolver = resolve_by_name('student', id=1)
+        resolver = self.resolve_by_name('student', id=1)
         allowed = ['get', 'put', 'delete']
+        self.assert_has_actions(allowed, resolver.func.actions)
 
-        self.assertEqual(len(allowed), len(resolver.func.actions))
-        for action in allowed:
-            self.assertIn(action, resolver.func.actions)
+    def resolve_by_name(self, name, **kwargs):
+        url = reverse(name, kwargs=kwargs)
+        return resolve(url)
+
+    def assert_has_actions(self, expected, actual):
+        self.assertEqual(len(expected), len(actual))
+        for action in expected:
+            self.assertIn(action, actual)
