fb58371a46069d89d3bdd6c0bc0a8bb6e89c66d5
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-23 20:30:59 -0300

    Fixed passing test

--- people/views.py
+++ people/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import StudentSerializer
+from .models import Student
 
 class StudentViewSet(viewsets.ModelViewSet):
     serializer_class = StudentSerializer
+    queryset = Student.objects.all()
