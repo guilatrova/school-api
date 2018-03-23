c3312dc4578f10e019dbce097b1b9a46ecad3a27
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-23 19:58:21 -0300

    Test resolves single url

--- people/tests.py
+++ people/tests.py
@@ -6,3 +6,7 @@ class PeopleUrl(TestCase):
     def test_resolves_students_list_url(self):
         resolver = resolve(reverse('students'))
         self.assertEqual(resolver.func.cls, views.StudentViewSet)
+
+    def test_resolves_students_single_url(self):
+        resolver = resolve(reverse('student', kwargs={'id':1}))
+        self.assertEqual(resolver.func.cls, views.StudentViewSet)
