3af3fb2054f023e9d9667aa91907acdf9ae5dce8
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-23 20:02:01 -0300

    Refactor tests

--- people/tests.py
+++ people/tests.py
@@ -2,11 +2,17 @@ from django.test import TestCase
 from django.urls import reverse, resolve
 from people import views
 
+def resolve_by_name(name, **kwargs):
+    url = reverse(name, kwargs=kwargs)
+    return resolve(url)
+
 class PeopleUrl(TestCase):
     def test_resolves_students_list_url(self):
-        resolver = resolve(reverse('students'))
+        resolver = resolve_by_name('students')
         self.assertEqual(resolver.func.cls, views.StudentViewSet)
 
     def test_resolves_students_single_url(self):
-        resolver = resolve(reverse('student', kwargs={'id':1}))
+        resolver = resolve_by_name('student', id=1)
         self.assertEqual(resolver.func.cls, views.StudentViewSet)
+
+    
