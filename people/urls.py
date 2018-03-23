70608fcf5f93c466825190b74b12c229979286db
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-23 20:07:32 -0300

    Test passing

--- people/urls.py
+++ people/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from people import views
 
 urlpatterns = [
-    path('', views.StudentViewSet.as_view({ 'get': 'list' }), name='students'),
+    path('', views.StudentViewSet.as_view({ 'get': 'list', 'post': 'create' }), name='students'),
     path('<int:id>/', views.StudentViewSet.as_view({ 'get': 'retrieve' }), name='student')
 ]
