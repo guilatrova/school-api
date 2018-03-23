5e9bd874524897875547a7ff3e4fdf4b09d4d996
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-23 20:22:37 -0300

    Passing serializer test

--- people/serializers.py
+++ people/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from people.models import Student
 
-class StudentSerializer:
-    pass
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ('id', 'name',)
