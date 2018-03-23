2c6e7b999076c3b50e79b1b4f26b3a5c6c5f5f00
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-23 19:43:24 -0300

    Added first url failing test

--- people/tests.py
+++ people/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from people import views
 
-# Create your tests here.
+class PeopleUrl(TestCase):
+    def test_resolves_students_list_url(self):
+        resolver = reverse('students')
+        self.assertEqual(resolver.func.cls, views.StudentViewSet)
