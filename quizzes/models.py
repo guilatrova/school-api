fab47c0fdadbc070b6076ec3ac290ceacfc51667
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-24 14:54:57 -0300

    Refactor + pending migration

--- quizzes/models.py
+++ quizzes/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Question(models.Model):
+class HasAnswerChoices:
     ANSWER_CHOICES = (
         ('A', 1),
         ('B', 2),
@@ -8,17 +8,11 @@ class Question(models.Model):
         ('D', 4),
     )
 
+class Question(models.Model, HasAnswerChoices):
     description = models.CharField(max_length=100)
-    correct_answer = models.PositiveSmallIntegerField(choices=ANSWER_CHOICES)
+    correct_answer = models.PositiveSmallIntegerField(choices=HasAnswerChoices.ANSWER_CHOICES)
 
-class Answer(models.Model):
-    ANSWER_CHOICES = (
-        ('A', 1),
-        ('B', 2),
-        ('C', 3),
-        ('D', 4),
-    )
-
+class Answer(models.Model, HasAnswerChoices):
     description = models.CharField(max_length=40)
-    choice = models.PositiveSmallIntegerField(choices=ANSWER_CHOICES)
+    choice = models.PositiveSmallIntegerField(choices=HasAnswerChoices.ANSWER_CHOICES)
     question = models.ForeignKey('Question', on_delete=models.CASCADE, related_name='answers')
