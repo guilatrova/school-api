f43507d8cc95350e07d96b38bebacc5189da396a
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-24 12:18:39 -0300

    Refactor improve quality + added docs

--- classes/views.py
+++ classes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from .serializers import SchoolClassSerializer, StudentEnrollmentSerializer
 from .models import SchoolClass, StudentEnrollment
 
@@ -7,7 +7,13 @@ class SchoolClassViewSet(viewsets.ModelViewSet):
     serializer_class = SchoolClassSerializer
     queryset = SchoolClass.objects.all()
 
-class StudentClassesViewSet(viewsets.ModelViewSet):
+class StudentClassesViewSet(mixins.CreateModelMixin, 
+                            mixins.ListModelMixin, 
+                            viewsets.GenericViewSet):
+    """
+    A viewset that only handles Create and List 
+    for student enrollment in classes.
+    """
     serializer_class = StudentEnrollmentSerializer
 
     def get_queryset(self):
