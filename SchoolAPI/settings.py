a071fe514e0a316a970bd4bdb8d7a5c7ad8b367b
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-24 09:26:45 -0300

    Added url tests for classes

--- SchoolAPI/settings.py
+++ SchoolAPI/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     #LOCAL MODULES
-    'people'
+    'people',
+    'classes'
 ]
 
 MIDDLEWARE = [
--- classes/tests.py
+++ classes/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from classes import views
 
-# Create your tests here.
+class ClassesUrlsTestCase(TestCase):
+    def test_resolves_list_url(self):
+        resolver = self.resolve_by_name('classes')
+        self.assertEqual(resolver.func.cls, views.ClassViewSet)
+
+    def resolve_by_name(self, name, **kwargs):
+        url = reverse(name, kwargs=kwargs)
+        return resolve(url)
