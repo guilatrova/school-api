bbb52ad1647e170c9fc6a694abef98c7ff1068d4
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-24 17:49:17 -0300

    Refactor

--- people/urls/urls_students.py
+++ people/urls/urls_students.py
@@ -5,11 +5,11 @@ from quizzes import views as quizzes_views
 
 list_actions = { 'get': 'list', 'post': 'create' }
 single_actions = { 'get': 'retrieve', 'put': 'update', 'delete': 'destroy' }
-classes_action = { 'get': 'list', 'post': 'create' }
+relation_actions = { 'get': 'list', 'post': 'create' }
 
 urlpatterns = [
     path('', views.StudentViewSet.as_view(list_actions), name='students'),
     path('<int:pk>/', views.StudentViewSet.as_view(single_actions), name='student'),
-    path('<int:student_id>/classes', classes_views.StudentClassesViewSet.as_view(classes_action), name='student-classes'),
-    path('<int:student_id>/assignments', quizzes_views.StudentAssignmentsViewSet.as_view(classes_action), name='student-assignments'),
+    path('<int:student_id>/classes', classes_views.StudentClassesViewSet.as_view(relation_actions), name='student-classes'),
+    path('<int:student_id>/assignments', quizzes_views.StudentAssignmentsViewSet.as_view(relation_actions), name='student-assignments'),
 ]
--- quizzes/views.py
+++ quizzes/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from .serializers import QuizSerializer
 from .models import Quiz
 
 class QuizViewSet(mixins.CreateModelMixin, 
-                  mixins.ListModelMixin, 
+                  mixins.ListModelMixin,
+                  mixins.RetrieveModelMixin,
                   viewsets.GenericViewSet):
     serializer_class = QuizSerializer
     queryset = Quiz.objects.all()
