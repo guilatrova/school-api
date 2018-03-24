642f25bf2166bae474f381914778b747ce193d38
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-24 15:21:01 -0300

    Refactor to make test DRYER

--- quizzes/tests.py
+++ quizzes/tests.py
@@ -17,36 +17,36 @@ class AnswerSerializerTestCase(TestCase):
 class QuestionSerializerTestCase(TestCase):
     def test_serializer_validates(self):
         answers_data = self.create_answers('django', 'flask', 'cherry', 'none')
-        question_data =  {
-            'description': 'Which is the best framework for perfectionists?',
-            'correct_answer': 'A',
-            'answers': answers_data
-        }
+        question_data =  question_data =  self.create_question(answers_data)
+        
         serializer = serializers.QuestionSerializer(data=question_data)
         self.assertTrue(serializer.is_valid())
 
     def test_answer_validates_should_have_4_answers_choices(self):
         answers_data = self.create_answers('just', 'three', 'answers')
-        question_data =  {
-            'description': 'This test allows less than four choices?',
-            'correct_answer': 'B',
-            'answers': answers_data
-        }
+        question_data =  self.create_question(answers_data)
+
         serializer = serializers.QuestionSerializer(data=question_data)
-        self.assertFalse(serializer.is_valid())
-        self.assertIn('answers', serializer.errors)
+        self.assert_has_error(serializer, 'answers')
 
     def test_answer_validates_should_not_allows_repetead_choices(self):
         answers_data = self.create_answers('yes', 'no', 'well...', 'I dont know')
         answers_data[1]['choice'] = 'A' #now we got two A choices
-        question_data = {
-            'description': 'This test allows same choice index?',
-            'correct_answer': 'B',
-            'answers': answers_data
-        }
+        question_data = self.create_question(answers_data)
+        
         serializer = serializers.QuestionSerializer(data=question_data)
+        self.assert_has_error(serializer, 'answers')
+
+    def assert_has_error(self, serializer, key):
         self.assertFalse(serializer.is_valid())
-        self.assertIn('answers', serializer.errors)
+        self.assertIn(key, serializer.errors)
+
+    def create_question(self, answers):
+        return {
+            'description': 'question',
+            'correct_answer': 'D',
+            'answers': answers
+        }
 
     def create_answers(self, *args):
         lst = []
