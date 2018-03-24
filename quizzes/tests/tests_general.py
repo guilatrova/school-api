4254754b1f264c216eb6fbb8621e87ae9a16849a
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-24 18:01:55 -0300

    Refactor - extracted serializers tests

---
+++ quizzes/tests/tests_general.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from quizzes import views, serializers, factories
+from quizzes.models import Quiz, Question, Answer, Assignment
+from people.models import Teacher, Student
+from classes.models import SchoolClass, StudentEnrollment
+from common.tests.mixins import UrlTestMixin
+from .helpers import SetupSchoolClassDataMixin, create_questions, create_question, create_answers
+
+class QuizUrlsTestCase(UrlTestMixin, TestCase):
+    list_name = 'quizzes'
+    single_name = 'quiz'
+    allowed_single = ['get']
+    allowed_list = ['get', 'post']
+    view = views.QuizViewSet
+
+class FactoriesTestCase(SetupSchoolClassDataMixin, TestCase):
+    def setUp(self):
+        answers = create_answers('A', 'B', 'C', 'D')
+        questions = [create_question(answers) for x in range(3)]
+        self.data = {
+            'school_class': self.school_class,
+            'questions': questions
+        }
+
+    def test_create_quiz(self):
+        factories.create_quiz(self.data)
+
+        self.assertEqual(Answer.objects.count(), 4 * 3)
+        self.assertEqual(Question.objects.count(), 3)
+        self.assertEqual(Quiz.objects.count(), 1)
+
--- quizzes/tests/tests_serializer.py
+++ quizzes/tests/tests_serializer.py
@@ -1,18 +1,10 @@
 from django.test import TestCase
 from unittest.mock import patch, MagicMock
-from quizzes import views, serializers, factories
+from quizzes import serializers, factories
 from quizzes.models import Quiz, Question, Answer, Assignment
 from people.models import Teacher, Student
 from classes.models import SchoolClass, StudentEnrollment
-from common.tests.mixins import UrlTestMixin
 from .helpers import SetupSchoolClassDataMixin, create_questions, create_question, create_answers
-
-class QuizUrlsTestCase(UrlTestMixin, TestCase):
-    list_name = 'quizzes'
-    single_name = 'quiz'
-    allowed_single = ['get']
-    allowed_list = ['get', 'post']
-    view = views.QuizViewSet
 
 class AnswerSerializerTestCase(TestCase):
     def test_serializer_validates(self):
@@ -94,20 +86,3 @@ class AssignmentSerializerTestCase(SetupSchoolClassDataMixin, TestCase):
         data = { 'quiz': self.quiz.id, 'enrollment': self.enrollment.id }
         serializer = serializers.AssignmentSerializer(data=data)
         self.assertTrue(serializer.is_valid(raise_exception=True))
-
-class FactoriesTestCase(SetupSchoolClassDataMixin, TestCase):
-    def setUp(self):
-        answers = create_answers('A', 'B', 'C', 'D')
-        questions = [create_question(answers) for x in range(3)]
-        self.data = {
-            'school_class': self.school_class,
-            'questions': questions
-        }
-
-    def test_create_quiz(self):
-        factories.create_quiz(self.data)
-
-        self.assertEqual(Answer.objects.count(), 4 * 3)
-        self.assertEqual(Question.objects.count(), 3)
-        self.assertEqual(Quiz.objects.count(), 1)
-
