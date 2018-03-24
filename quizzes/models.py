236137bfc69c5ee3d017691c7badd87e59b5c581
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-24 14:23:49 -0300

    Serializer test is passing

--- quizzes/models.py
+++ quizzes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Answer(models.Model):
+    ANSWER_CHOICES = (
+        ('A', 1),
+        ('B', 2),
+        ('C', 3),
+        ('D', 4),
+    )
+
+    description = models.CharField(max_length=40)
+    choice = models.PositiveSmallIntegerField(choices=ANSWER_CHOICES)
--- quizzes/serializers.py
+++ quizzes/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+from .models import Answer
 
 class AnswerSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Answer
+        fields = ('description', 'choice')
