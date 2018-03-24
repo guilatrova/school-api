f2556ab3c60492be314bc62743e7c277750bda68
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-24 09:56:30 -0300

    Added failing test for Classes Serializers

--- classes/tests.py
+++ classes/tests.py
@@ -1,9 +1,20 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from classes import views
+from people.models import Teacher
+from classes import views, serializers
 from common.tests.mixins import UrlTestMixin
 
 class ClassesUrlsTestCase(UrlTestMixin, TestCase):
     list_name = 'classes'
     single_name = 'class'
     view = views.ClassViewSet
+
+class ClassSerializerTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.teacher = Teacher.objects.create(name='Guilherme Latrova')
+
+    def test_serializer_validates(self):
+        data = { 'name': 'Python with TDD', 'teacher': self.teacher }
+        serializer = serializers.ClassSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
