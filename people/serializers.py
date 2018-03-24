8d5acc5fa2a10be162cdac1b00d6e7867364e374
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-24 08:12:49 -0300

    Added passing Teacher test

--- people/serializers.py
+++ people/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from people.models import Student
+from people.models import Student, Teacher
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
+        fields = ('id', 'name',)
+
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
         fields = ('id', 'name',)
