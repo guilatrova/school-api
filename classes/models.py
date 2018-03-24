09a7c647abeae1a2553850368a32af0340702b9d
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-24 11:00:06 -0300

    Refactor - models import really needed

--- classes/models.py
+++ classes/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from people.models import Teacher
 
 class Class(models.Model):
     name = models.CharField(max_length=100)
-    teacher = models.ForeignKey(Teacher, on_delete=models.PROTECT)
+    teacher = models.ForeignKey('people.Teacher', on_delete=models.PROTECT)
